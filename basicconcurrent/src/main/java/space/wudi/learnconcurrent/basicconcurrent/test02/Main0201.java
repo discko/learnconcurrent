512704a9fc5fa779200c7a6e4f68624d53bbc28f
discko <discko@qq.com>
2021-02-02 18:25:50 +0800

    add test case to make field visible among threads without keyword volatile

--- basicconcurrent/src/main/java/space/wudi/learnconcurrent/basicconcurrent/test02/Main0201.java
+++ basicconcurrent/src/main/java/space/wudi/learnconcurrent/basicconcurrent/test02/Main0201.java
@@ -3,13 +3,13 @@ package space.wudi.learnconcurrent.basicconcurrent.test02;
 import java.util.concurrent.TimeUnit;
 
 /**
- * try to use key word volatile to prevent thread cache
+ * try to prevent thread cache WITH keyword volatile
  */
 public class Main0201 {
     @SuppressWarnings("all")
     static class MyAction{
-        boolean flag1 = true;
-        volatile boolean flag2 = true;
+        volatile boolean flag1 = true;
+        boolean flag2 = true;
         void fun1(){
             System.out.println(Thread.currentThread().getName()+" fun1 loop started");
             while (flag1) {
@@ -29,18 +29,18 @@ public class Main0201 {
     public static void main(String[] args) {
         MyAction a1 = new MyAction();
         MyAction a2 = new MyAction();
-        Thread t1 = new Thread(a1::fun1);
-        Thread t2 = new Thread(a2::fun2);
+        Thread t1 = new Thread(a1::fun1, "Thread-1");
+        Thread t2 = new Thread(a2::fun2, "Thread-2");
         t1.start();
         t2.start();
         try {
-            TimeUnit.SECONDS.sleep(1);
+            TimeUnit.MILLISECONDS.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         a1.flag1 = false;
         System.out.println("change a1.flag to " + a1.flag1);
         a2.flag2 = false;
-        System.out.println("change a2.flag to " + a2.flag1);
+        System.out.println("change a2.flag to " + a2.flag2);
     }
 }
---
+++ basicconcurrent/src/main/java/space/wudi/learnconcurrent/basicconcurrent/test02/Main0204.java
@@ -0,0 +1,63 @@
+package space.wudi.learnconcurrent.basicconcurrent.test02;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.LinkedList;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * try to prevent thread cache WITHOUT keyword volatile
+ */
+public class Main0204 {
+    @SuppressWarnings("all")
+    static class MyAction{
+        /*volatile */boolean flag = true;
+        Lock lock = new ReentrantLock();
+        StringBuffer stringBuffer = new StringBuffer();
+        StringBuilder stringBuilder = new StringBuilder("123");
+        LinkedList<Integer> list = new LinkedList<>();
+        LinkedBlockingDeque<Integer> lbq = new LinkedBlockingDeque<>();
+        AtomicInteger integer = new AtomicInteger(1);
+        int[] src = new int[16];
+        int[] tgt = new int[16];
+        int a=0;
+        void fun() {
+            System.out.println(Thread.currentThread().getName()+" fun1 loop started");
+
+            while (flag) {
+//                lock.lock(); lock.unlock();
+//                Thread.yield();
+//                stringBuffer.append("a");
+//                stringBuilder.append("a");
+//                list.add(1);
+//                lbq.add(1);
+//                integer.addAndGet(1);
+//                a++;
+//                System.out.println("running");
+//                System.arraycopy(src, 0, tgt, 0, src.length);
+//                stringBuilder.append('a');
+//                stringBuilder.setCharAt(0, 'a');
+            }
+            System.out.println(Thread.currentThread().getName()+" fun1 loop stopped");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MyAction a = new MyAction();
+        Thread t = new Thread(a::fun, "Thread-1");
+        t.start();
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        a.flag = false;
+//        System.out.println("change a1.flag to " + a1.flag1);
+
+    }
+}
